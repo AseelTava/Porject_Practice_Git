c8f4aa5b9997b000cdb6cefef06f66e25814eb15
casho <aseelhussein@aol.com>
2022-08-09 12:28:46 -0400

    Login page completed

--- src/Day2/Login.java
+++ src/Day2/Login.java
@@ -3,9 +3,11 @@ package Day2;
 public class Login {
     // page is created
 
-    // i am workong on login
+    // i am working on login
 
     // i am about to complete
 
     // completed
+
+    //review
 }
